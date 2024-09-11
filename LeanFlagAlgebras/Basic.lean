36cf377f79a387011510a870cbd776245fb4163e
taeyool <jgyw0910@kaist.ac.kr>
2024-09-11 16:55:28 +0900

    add combinations

--- LeanFlagAlgebras/Basic.lean
+++ LeanFlagAlgebras/Basic.lean
@@ -1,1 +1,15 @@
-def hello := "world"
+import Mathlib.Combinatorics.SimpleGraph.Subgraph
+import Mathlib.Data.Set.Finite
+import Mathlib.Data.Nat.Choose.Basic
+import Mathlib.Data.Finset.Basic
+
+open Set
+
+def combinations (V : Finset α) (ℓ : ℕ) : Finset (Finset α) :=
+  (V.powerset).filter fun V' ↦ V'.card = ℓ
+
+theorem card_comb (V : Finset α) (ℓ : ℕ) : (combinations V ℓ).card = V.card.choose ℓ := by
+  sorry
+
+def p (M : SimpleGraph α) (N : SimpleGraph β) [DecidableRel M.Adj] [DecidableRel N.Adj] : ℚ :=
+  sorry
