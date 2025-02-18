edcf9cc5c47d89609acae6df1417251f7c01f0e8
Hongseok Yang <hongseok00@gmail.com>
2025-02-18 12:39:57 +0900

    I tried to follow the usual strategy to prove quotSubgraphPairDensity_eq_sum_density_prods, but I got stuck.

--- LeanFlagAlgebras/SubgraphDensity.lean
+++ LeanFlagAlgebras/SubgraphDensity.lean
@@ -952,7 +952,26 @@ lemma quotSubgraphPairDensity_empty
   rcases Quotient.exists_rep G with ⟨Grep, hGrep⟩
   rw [← hHrep, ← hGrep]
   apply subgraphPairDensity_empty
-  -- exact subgraphPairDensity_empty Hrep Grep
+
+lemma subgraphPairDensity_eq_sum_density_prods
+    (H₁ : SimpleGraph (Fin ℓ₁)) (H₂ : SimpleGraph (Fin ℓ₂)) (G : SimpleGraph (Fin ℓ))
+    {ℓ' : ℕ} (hℓ' : ℓ₁ + ℓ₂ ≤ ℓ') (hℓ : ℓ' ≤ ℓ)
+    : subgraphPairDensity H₁ H₂ G
+      = ∑ (G' : QuotSimpleGraph (Fin ℓ')), subgraphPairDensityLifted H₁ H₂ G' * quotSubgraphDensity G' ⟦G⟧
+  := by
+  rw [subgraphPairDensityLifted, quotSubgraphDensity]
+  have h₁ : subgraphPairDensity H₁ H₂ G
+            = ∑ (G' : QuotSimpleGraph (Fin ℓ')), subgraphPairDensity H₁ H₂ (Quot.out G') * subgraphDensity (Quot.out G') G
+    := sorry
+  have h₂ : ∀ G' : QuotSimpleGraph (Fin ℓ'),
+              subgraphPairDensity H₁ H₂ (Quot.out G') * subgraphDensity (Quot.out G') G
+              = subgraphPairDensityLifted H₁ H₂ G' * quotSubgraphDensity G' ⟦G⟧
+    := sorry
+  rw [h₁]
+  apply congrArg
+  ext G'
+  exact h₂ G'
+
 
 theorem quotSubgraphPairDensity_eq_sum_density_prods
     (H₁ : QuotSimpleGraph (Fin ℓ₁)) (H₂ : QuotSimpleGraph (Fin ℓ₂)) (G : QuotSimpleGraph (Fin ℓ))
@@ -960,6 +979,10 @@ theorem quotSubgraphPairDensity_eq_sum_density_prods
     : quotSubgraphPairDensity H₁ H₂ G
       = ∑ (G' : QuotSimpleGraph (Fin ℓ')), quotSubgraphPairDensity H₁ H₂ G' * quotSubgraphDensity G' G
   := by
-  sorry
+  rcases Quotient.exists_rep H₁ with ⟨H₁rep, hH₁rep⟩
+  rcases Quotient.exists_rep H₂ with ⟨H₂rep, hH₂rep⟩
+  rcases Quotient.exists_rep G with ⟨Grep, hGrep⟩
+  rw [← hH₁rep, ← hH₂rep, ← hGrep]
+  exact subgraphPairDensity_eq_sum_density_prods H₁rep H₂rep Grep hℓ' hℓ
 
 alias density_chain_rule := quotSubgraphPairDensity_eq_sum_density_prods
