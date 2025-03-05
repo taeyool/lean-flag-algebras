e5328382702f251b7cb5afd2ee80d0d5af9fd3de
taeyool <jgyw0910@kaist.ac.kr>
2025-03-05 17:19:42 +0900

    Define flag (quotient labeled graph)

--- LeanFlagAlgebras/FlagDef.lean
+++ LeanFlagAlgebras/FlagDef.lean
@@ -1,14 +1,18 @@
 import Mathlib.Combinatorics.SimpleGraph.Maps
 
-namespace Flag
-
 abbrev FlagType := SimpleGraph
 
-structure LabeledGraph [Fintype T] (σ : FlagType T) (V : Type) where
+@[ext]
+structure LabeledGraph {T : Type} [Fintype T] (σ : FlagType T) (V : Type) where
   graph : SimpleGraph V
   type_embed : σ ↪g graph
 
-structure LabeledGraphIso [Fintype T] {σ : FlagType T} {V W : Type}
+instance {T : Type} [Fintype T] {σ : FlagType T} {V : Type} [Fintype V] [DecidableEq V]
+    : Fintype (LabeledGraph σ V) where
+  elems := sorry
+  complete := sorry
+
+structure LabeledGraphIso {T : Type} [Fintype T] {σ : FlagType T} {V W : Type}
   (G : LabeledGraph σ V) (G' : LabeledGraph σ W) where
   graph_iso : G.graph ≃g G'.graph
   type_preserve : graph_iso ∘ G.type_embed = G'.type_embed
@@ -17,8 +21,8 @@ infixl:50 " ≃f " => LabeledGraphIso
 
 namespace LabeledGraphIso
 
-variable [Fintype T] {σ : FlagType T}
-  {G : LabeledGraph σ V} {G' : LabeledGraph σ W} {G'' : LabeledGraph σ U}
+variable {T : Type} [Fintype T] {σ : FlagType T} {V W U : Type}
+variable {G : LabeledGraph σ V} {G' : LabeledGraph σ W} {G'' : LabeledGraph σ U}
 
 def refl : G ≃f G := sorry
 
@@ -28,4 +32,50 @@ def trans (h : G ≃f G') (h' : G' ≃f G'') : G ≃f G'' := sorry
 
 end LabeledGraphIso
 
-end Flag
+variable {T : Type} [Fintype T] {V : Type}
+
+def flagEqv {σ : FlagType T} (G G' : LabeledGraph σ V) : Prop
+  :=
+  Nonempty (G ≃f G')
+
+infixl:50 " ∼f " => flagEqv
+
+theorem flagEqv.refl {σ : FlagType T} (G : LabeledGraph σ V)
+    : G ∼f G
+  :=
+  Nonempty.intro LabeledGraphIso.refl
+
+theorem flagEqv.symm {σ : FlagType T}
+    : ∀ {G G' : LabeledGraph σ V}, G ∼f G' → G' ∼f G
+  := by
+  intro G G' h
+  have G_iso : G ≃f G' := Classical.choice h
+  exact Nonempty.intro G_iso.symm
+
+theorem flagEqv.trans {σ : FlagType T}
+    : ∀ {G G' G'' : LabeledGraph σ V}, G ∼f G' → G' ∼f G'' → G ∼f G''
+  := by
+  intro G G' G'' h h'
+  have G_iso : G ≃f G' := Classical.choice h
+  have G'_iso : G' ≃f G'' := Classical.choice h'
+  exact Nonempty.intro (G_iso.trans G'_iso)
+
+variable [DecidableEq T]
+
+instance labededGraphSetoid (σ : FlagType T) (V : Type) [DecidableEq V]
+    : Setoid (LabeledGraph σ V)
+  where
+    r     := flagEqv
+    iseqv := {
+      refl  := flagEqv.refl,
+      symm  := flagEqv.symm,
+      trans := flagEqv.trans
+    }
+
+def Flag (σ : FlagType T) (V : Type) [DecidableEq V] : Type :=
+  Quotient (labededGraphSetoid σ V)
+
+noncomputable instance FlagFintype {σ : FlagType T} {V : Type} [Fintype V] [DecidableEq V] : Fintype (Flag σ V)
+  := by
+  classical
+  exact Quotient.fintype (labededGraphSetoid σ V)
