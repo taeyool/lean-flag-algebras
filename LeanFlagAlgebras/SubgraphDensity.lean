d03297ca14a0700c4acf23f800c2c79c3cc1f077
Hongseok Yang <hongseok00@gmail.com>
2025-03-06 11:30:59 +0900

    minor progress

--- LeanFlagAlgebras/SubgraphDensity.lean
+++ LeanFlagAlgebras/SubgraphDensity.lean
@@ -1125,7 +1125,7 @@ noncomputable def subgraphPairSet
   { (G₁, G₂) : Subgraph G × Subgraph G |
     G₁.IsInduced ∧ Nonempty (Subgraph.coe G₁ ≃g H₁) ∧
     G₂.IsInduced ∧ Nonempty (Subgraph.coe G₂ ≃g H₂) ∧
-    G₁.verts ∩ G₂.verts = ∅ }.toFinset
+    G₁.verts ∩ G₂.verts = ∅ }
 
 def subgraphFromIso
     {G : SimpleGraph V} {H : SimpleGraph W}
@@ -1236,11 +1236,30 @@ noncomputable def subgraphPairSet_iso_union_quotSimpleGraphSet
   := by
   let ℓ₁₂ := ℓ₁ + ℓ₂
   let S₀ := subgraphPairSet H₁ H₂ G
-  let S₁ := { (G₁, G₂, G₁₂) : Subgraph G × Subgraph G × Subgraph G | (G₁, G₂) ∈ S₀ ∧ G₁₂ = G₁ ⊔ G₂}
-  have f_S₀_S₁ : S₀ ≃ S₁ := sorry
-  let S₂ := Σ F : QuotSimpleGraph (Fin (ℓ₁ + ℓ₂)), subgraphPairSet H₁ H₂ F.out × subgraphSet F.out G
-  have f_S₁_S₂ : S₁ ≃ S₂ := sorry
-  exact f_S₀_S₁.trans f_S₁_S₂
+  let S₁ := { (G₁, G₂) : Subgraph G × Subgraph G |
+                G₁.IsInduced ∧ Nonempty (Subgraph.coe G₁ ≃g H₁) ∧
+                G₂.IsInduced ∧ Nonempty (Subgraph.coe G₂ ≃g H₂) ∧
+                G₁.verts ∩ G₂.verts = ∅ }
+  let S₂ := { (G₁, G₂, G₁₂) : Subgraph G × Subgraph G × Subgraph G | (G₁, G₂) ∈ S₁ ∧ G₁₂ = G₁ ⊔ G₂}
+  let S₃ := Σ F : QuotSimpleGraph (Fin (ℓ₁ + ℓ₂)), subgraphPairSet H₁ H₂ F.out × subgraphSet F.out G
+  have f_S₀_S₁ : S₀ ≃ S₁ := by
+    have : S₀ = S₁ := by
+      ext ⟨G₁, G₂⟩
+      dsimp [S₀, S₁, subgraphPairSet]
+      simp
+    exact Equiv.subtypeEquivProp this
+  have f_S₁_S₂ : S₁ ≃ S₂ := {
+    toFun := fun ⟨⟨G₁, G₂⟩, h_G₁_G₂⟩ => by exact ⟨⟨G₁, G₂, G₁ ⊔ G₂⟩, h_G₁_G₂, rfl⟩
+    invFun := fun ⟨⟨G₁, G₂, G₁₂⟩, h_G₁_G₂, h_G₁₂⟩ => ⟨⟨G₁, G₂⟩, h_G₁_G₂⟩
+    left_inv := by
+      intro ⟨⟨G₁, G₂⟩, h_G₁_G₂⟩
+      simp
+    right_inv := by
+      intro ⟨⟨G₁, G₂, G₁₂⟩, h_G₁_G₂, h_G₁₂⟩
+      simp [h_G₁₂]
+  }
+  have f_S₂_S₃ : S₂ ≃ S₃ := sorry
+  exact (f_S₀_S₁.trans f_S₁_S₂).trans f_S₂_S₃
 
 def multichoose (n m₁ m₂ : ℕ) : ℕ :=
   n.choose m₁ * (n - m₁).choose m₂
