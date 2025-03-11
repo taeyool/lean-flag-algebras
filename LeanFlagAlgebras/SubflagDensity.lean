b9e5932dcc6fdd957c5a108c0de1b3458bc0667f
taeyool <jgyw0910@kaist.ac.kr>
2025-03-11 16:37:14 +0900

    Define subflagDensity (quotient version of labeledSubgraphDensity)

--- LeanFlagAlgebras/SubflagDensity.lean
+++ LeanFlagAlgebras/SubflagDensity.lean
@@ -24,3 +24,30 @@ noncomputable def labeledSubgraphDensity
   let G_size := Fintype.card W
   let num_of_all_induced_subgraph := (G_size - σ_size).choose (H_size - σ_size)
   labeledSubgraph_cnt / num_of_all_induced_subgraph
+
+lemma labeledSubgraphDensity_respects_eqv_on_G
+    (H : LabeledGraph σ V) {G G' : LabeledGraph σ W} (φ : G ≃f G')
+    : labeledSubgraphDensity H G = labeledSubgraphDensity H G'
+  :=
+  sorry
+
+noncomputable def labeledSubgraphDensityLifted
+    (H : LabeledGraph σ V) : Flag σ W → ℚ
+  := by
+  apply Quot.lift (fun G : LabeledGraph σ W => labeledSubgraphDensity H G)
+  intro _ _ G_eqv
+  exact labeledSubgraphDensity_respects_eqv_on_G H (Classical.choice G_eqv)
+
+lemma labeledSubgraphDensityLifted_respects_eqv
+    (H H' : LabeledGraph σ V) (φ : H ≃f H') (G : Flag σ W)
+    : labeledSubgraphDensityLifted H G = labeledSubgraphDensityLifted H' G
+  :=
+  sorry
+
+noncomputable def subflagDensity
+    : Flag σ V → Flag σ W → ℚ
+  := by
+  apply Quot.lift labeledSubgraphDensityLifted
+  intro H H' H_eqv
+  ext G
+  exact labeledSubgraphDensityLifted_respects_eqv H H' (Classical.choice H_eqv) G
