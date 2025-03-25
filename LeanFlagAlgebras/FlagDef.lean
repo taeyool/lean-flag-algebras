a5e0dab7d78971988acc3053144be5d43ea8a220
taeyool <jgyw0910@kaist.ac.kr>
2025-03-25 16:48:53 +0900

    Add list notation for FlagList

--- LeanFlagAlgebras/FlagDef.lean
+++ LeanFlagAlgebras/FlagDef.lean
@@ -219,10 +219,24 @@ def QuotLabeledGraphList (σ : FlagType T) (t : ℕ) (V : Fin t → Type) : Type
 
 abbrev FlagList (σ : FlagType T) (t : ℕ) (V : Fin t → Type) := ∀ (i : Fin t), Flag σ (V i)
 
-def Flag.toSingletonList {σ : FlagType T} {V : Type} (F : Flag σ V)
+def flagToList {σ : FlagType T} {V : Type} (F : Flag σ V)
     : FlagList σ 1 (fun _ => V)
   :=
   fun _ => F
+
+def flagPairToList {σ : FlagType T} {V W : Type} (F : Flag σ V) (G : Flag σ W)
+    : FlagList σ 2 (fun i => match i with | 0 => V | 1 => W)
+  :=
+  fun i => match i with | 0 => F | 1 => G
+
+def flagTripleToList {σ : FlagType T} {V W U : Type} (F : Flag σ V) (G : Flag σ W) (H : Flag σ U)
+    : FlagList σ 3 (fun i => match i with | 0 => V | 1 => W | 2 => U)
+  :=
+  fun i => match i with | 0 => F | 1 => G | 2 => H
+
+notation "[" F "]ᶠ" => (flagToList F)
+notation "[" F "," G "]ᶠ" => (flagPairToList F G)
+notation "[" F "," G "," H "]ᶠ" => (flagTripleToList F G H)
 
 instance fintypeSingletonList {V : Type} [Fintype V]
     : @FintypeList 1 (fun _ => V)
@@ -233,6 +247,26 @@ instance DecidableEqSingletonList {V : Type} [DecidableEq V]
     : @DecidableEqList 1 (fun _ => V)
   :=
   { decidable_eq_all := fun _ ↦ inferInstance }
+
+instance fintypePairList {V W : Type} [Fintype V] [Fintype W]
+    : @FintypeList 2 (fun i => match i with | 0 => V | 1 => W)
+  :=
+  { fintype_all := fun i => match i with | 0 => inferInstance | 1 => inferInstance }
+
+instance DecidableEqPairList {V W : Type} [DecidableEq V] [DecidableEq W]
+    : @DecidableEqList 2 (fun i => match i with | 0 => V | 1 => W)
+  :=
+  { decidable_eq_all := fun i => match i with | 0 => inferInstance | 1 => inferInstance }
+
+instance fintypeTripleList {V W U : Type} [Fintype V] [Fintype W] [Fintype U]
+    : @FintypeList 3 (fun i => match i with | 0 => V | 1 => W | 2 => U)
+  :=
+  { fintype_all := fun i => match i with | 0 => inferInstance | 1 => inferInstance | 2 => inferInstance }
+
+instance DecidableEqTripleList {V W U : Type} [DecidableEq V] [DecidableEq W] [DecidableEq U]
+    : @DecidableEqList 3 (fun i => match i with | 0 => V | 1 => W | 2 => U)
+  :=
+  { decidable_eq_all := fun i => match i with | 0 => inferInstance | 1 => inferInstance | 2 => inferInstance }
 
 noncomputable instance eqv_QuotLabeledGraphList_FlagList (σ : FlagType T) (t : ℕ) (V : Fin t → Type)
     : QuotLabeledGraphList σ t V ≃ FlagList σ t V where
