9f94b61731ef0cbfb3e92a06a655dc5f7e92b243
rhaxod22 <hun57@kaist.ac.kr>
2025-03-31 21:55:18 +0900

    Struggling with proof for HEq

--- LeanFlagAlgebras/SubflagDensity.lean
+++ LeanFlagAlgebras/SubflagDensity.lean
@@ -47,8 +47,7 @@ def predIsolabeledH
 lemma predIsolabeldH_related
     {G₀ : LabeledGraph σ V} {G₁ : LabeledGraph σ W} (φ : G₀ ≃f G₁) (H : LabeledGraph σ U)
     : relOfPredOnlabeledfSubgraph φ (predIsolabeledH H G₀) (predIsolabeledH H G₁)
-  := by
-  sorry
+  := by sorry
 
 def inducedlabeledSubgraph
     {σ : FlagType T} (G : LabeledGraph σ V) (S : Set V) : {G' : LabeledSubgraph σ G // G'.IsInduced}
@@ -235,7 +234,7 @@ lemma inducedlabeledSubgraph_related
           exact h_uv_G₀
 
 noncomputable def isoSetOfInducedlabeledSubgraph
-    {G₀ : LabeledGraph σ V} {G₁ : LabeledGraph σ W} (φ : G₀ ≃f G₁)
+    {σ : FlagType T} {G₀ : LabeledGraph σ V} {G₁ : LabeledGraph σ W} (φ : G₀ ≃f G₁)
     (p₀ : LabeledSubgraph σ G₀ → Prop) (p₁ : LabeledSubgraph σ G₁ → Prop)
     (h_rel : relOfPredOnlabeledfSubgraph φ p₀ p₁) (h_rel_inv : relOfPredOnlabeledfSubgraph φ.symm p₁ p₀)
     : { G' : LabeledSubgraph σ G₀ | G'.IsInduced ∧ p₀ G' } ≃ { G' : LabeledSubgraph σ G₁ | G'.IsInduced ∧ p₁ G' }
@@ -309,7 +308,10 @@ noncomputable def isoSetOfInducedlabeledSubgraph
             · sorry
             · sorry
       · simp
+        cases' H₀.type_embed with embed map_rel_iff''
+        obtain ⟨toFun', inj''⟩ := embed
         sorry
+
     have h_rightinv : Function.RightInverse f_inv f := by
       rintro ⟨H₁, ⟨h_ind₁, h_p₁⟩⟩
       dsimp [f, f_inv, inducedlabeledSubgraph]
