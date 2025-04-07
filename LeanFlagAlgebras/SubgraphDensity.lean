8286d11a6d518851078c5d8247af692e34817527
Hongseok Yang <hongseok00@gmail.com>
2025-04-07 13:56:00 +0900

    tried expliicit eta expansion, but does not work.

--- LeanFlagAlgebras/SubgraphDensity.lean
+++ LeanFlagAlgebras/SubgraphDensity.lean
@@ -1701,9 +1701,13 @@ noncomputable def subgraphPairSet_iso_union_quotSimpleGraphSet
         have : res_forward.2.2.1 = G₁₂ := by
           dsimp [res_forward, f_S₃_S₄_forward]
         have : res_backward.1.2.2.1 = res_forward.2.2.1 := by
-          dsimp [res_backward, f_S₃_S₄_backward]
+          have h_res_backward : res_backward = f_S₃_S₄_backward ⟨res_forward.1, ⟨⟨res_forward.2.1.1.1, res_forward.2.1.1.2⟩, res_forward.2.1.2⟩, ⟨res_forward.2.2.1, res_forward.2.2.2⟩⟩
+            := by simp
+          rw [h_res_backward]
+          dsimp [f_S₃_S₄_backward]
+          simp_all
           sorry
-        simp_all
+        sorry
       sorry
     right_inv := by
       intro ⟨F, ⟨⟨G₁, G₂⟩, h_G₁_G₂_F⟩, ⟨G₁₂, h_G₁₂_F⟩⟩
