2122eb5edc2279c0564e907f5d10ac71d9b21881
Hongseok Yang <hongseok00@gmail.com>
2025-04-07 12:18:27 +0900

    struggling.

--- LeanFlagAlgebras/SubgraphDensity.lean
+++ LeanFlagAlgebras/SubgraphDensity.lean
@@ -1686,30 +1686,24 @@ noncomputable def subgraphPairSet_iso_union_quotSimpleGraphSet
     invFun := f_S₃_S₄_backward
     left_inv := by
       intro ⟨⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩, ⟨⟨h_G₁_ind, h_G₁_iso_H₁, h_G₂_ind, h_G₂_iso_H₂, h_G₁_G₂_disj⟩, h_G₁₂, h_F⟩⟩
-      let F_G₁_G₂_G₁₂ : QuotSimpleGraph (Fin (ℓ₁ + ℓ₂)) × G.Subgraph × G.Subgraph × {G' : G.Subgraph | G'.IsInduced }
-        := ⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩
-      let h_G₁_G₂_F : ⟨G₁, G₂⟩ ∈ S₁ ∧ ⟨G₁₂, h_G₁₂_ind⟩ = inducedSubgraph G (G₁.verts ∪ G₂.verts) ∧ Nonempty (F.out ≃g (G₁₂ : Subgraph G).coe)
-        := ⟨⟨h_G₁_ind, h_G₁_iso_H₁, h_G₂_ind, h_G₂_iso_H₂, h_G₁_G₂_disj⟩, h_G₁₂, h_F⟩
-      have h_eq_F : (f_S₃_S₄_backward (f_S₃_S₄_forward ⟨⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩, h_G₁_G₂_F⟩)).1.1
-                      = F
-        := by
-          dsimp [f_S₃_S₄_forward, f_S₃_S₄_backward]
-          simp_all only [Set.mem_setOf_eq, Set.coe_setOf, S₃, S₁]
-          obtain ⟨fst, snd⟩ := F_G₁_G₂_G₁₂
-          obtain ⟨left, right⟩ := h_G₁_G₂_F
-          obtain ⟨fst_1, snd⟩ := snd
-          obtain ⟨left_1, right⟩ := right
-          obtain ⟨fst_2, snd⟩ := snd
-          obtain ⟨val, property⟩ := snd
-          simp_all only [Set.mem_setOf_eq, and_self, S₁]
-          split
-          rename_i x h_G₁₂_ind_1 h_G₁₂_iso heq
-          simp_all only [and_self]
-      have h_eq_G₁ : (f_S₃_S₄_backward (f_S₃_S₄_forward ⟨⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩, h_G₁_G₂_F⟩)).1.2.1
-                     = G₁
-        := by
-          dsimp [f_S₃_S₄_forward, f_S₃_S₄_backward]
+      show (f_S₃_S₄_backward (f_S₃_S₄_forward ⟨⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩, ⟨⟨h_G₁_ind, h_G₁_iso_H₁, h_G₂_ind, h_G₂_iso_H₂, h_G₁_G₂_disj⟩, h_G₁₂, h_F⟩⟩))
+        = ⟨⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩, ⟨⟨h_G₁_ind, h_G₁_iso_H₁, h_G₂_ind, h_G₂_iso_H₂, h_G₁_G₂_disj⟩, h_G₁₂, h_F⟩⟩
+      let res_forward := f_S₃_S₄_forward ⟨⟨F, G₁, G₂, ⟨G₁₂, h_G₁₂_ind⟩⟩, ⟨⟨h_G₁_ind, h_G₁_iso_H₁, h_G₂_ind, h_G₂_iso_H₂, h_G₁_G₂_disj⟩, h_G₁₂, h_F⟩⟩
+      let res_backward := f_S₃_S₄_backward res_forward
+      have h_eq_F : res_backward.1.1 = F := by
+        have : res_forward.1 = F := by
+          dsimp [res_forward, f_S₃_S₄_forward]
+        have : res_backward.1.1 = res_forward.1 := by
+          dsimp [res_backward, f_S₃_S₄_backward]
+          aesop
+        simp_all
+      have h_eq_G₁ : res_backward.1.2.2.1 = G₁₂ := by
+        have : res_forward.2.2.1 = G₁₂ := by
+          dsimp [res_forward, f_S₃_S₄_forward]
+        have : res_backward.1.2.2.1 = res_forward.2.2.1 := by
+          dsimp [res_backward, f_S₃_S₄_backward]
           sorry
+        simp_all
       sorry
     right_inv := by
       intro ⟨F, ⟨⟨G₁, G₂⟩, h_G₁_G₂_F⟩, ⟨G₁₂, h_G₁₂_F⟩⟩
