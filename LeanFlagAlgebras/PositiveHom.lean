374948e5f68c5e978da31c0358f5c1ba500bc484
taeyool <jgyw0910@kaist.ac.kr>
2025-05-08 16:54:48 +0900

    Defined positive homomorphism space

---
+++ LeanFlagAlgebras/PositiveHom.lean
@@ -0,0 +1,31 @@
+import «LeanFlagAlgebras».FlagAlgebra
+
+open FlagAlgebras
+
+variable {n₀ : ℕ} {σ : FlagType (Fin n₀)}
+
+abbrev Hom (σ : FlagType (Fin n₀))
+  :=
+  FlagAlgebra σ →ₐ[ℝ] ℝ
+
+def positiveHom (σ : FlagType (Fin n₀)) : Type
+  :=
+  { φ : Hom σ // ∀ (F : FinFlag σ), φ ⟦unitVector F⟧ ≥ 0 }
+
+instance : FunLike (positiveHom σ) (FlagAlgebra σ) ℝ where
+  coe := fun φ => φ.val
+  coe_injective' f g h := by
+    rcases f with ⟨_, _⟩
+    rcases g with ⟨_, _⟩
+    simp at h
+    congr
+
+def semanticCone (σ : FlagType (Fin n₀)) : Set (FlagAlgebra σ) :=
+  { f : FlagAlgebra σ | ∀ (φ : positiveHom σ), φ f ≥ 0 }
+
+instance : LE (FlagAlgebra σ) where
+  le := fun f g => g - f ∈ semanticCone σ
+
+instance : Preorder (FlagAlgebra σ) where
+  le_refl := sorry
+  le_trans := sorry
