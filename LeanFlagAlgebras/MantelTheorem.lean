40871902da479a8bf023d803402440123c93c4bf
taeyool <jgyw0910@kaist.ac.kr>
2025-05-15 18:10:38 +0900

    More progress

--- LeanFlagAlgebras/MantelTheorem.lean
+++ LeanFlagAlgebras/MantelTheorem.lean
@@ -62,27 +62,27 @@ def K3_labeledGraph : LabeledGraph ∅ₜ (Fin 3) where
   graph := K3_graph
   type_embed := RelEmbedding.ofIsEmpty ∅ₜ.Adj K3_graph.Adj
 
-/-- a non-edge --/
+/-- a non-edge -/
 noncomputable def O2 : FlagAlgebra ∅ₜ :=
   ⟦unitVector ⟨2, ⟦O2_labeledGraph⟧⟩⟧
 
-/-- an edge --/
+/-- an edge -/
 noncomputable def K2 : FlagAlgebra ∅ₜ :=
   ⟦unitVector ⟨2, ⟦K2_labeledGraph⟧⟩⟧
 
-/-- an empty graph with 3 vertices --/
+/-- an empty graph with 3 vertices -/
 noncomputable def O3 : FlagAlgebra ∅ₜ :=
   ⟦unitVector ⟨3, ⟦O3_labeledGraph⟧⟩⟧
 
-/-- an edge and an isolated vertex --/
+/-- an edge and an isolated vertex -/
 noncomputable def E3 : FlagAlgebra ∅ₜ :=
   ⟦unitVector ⟨3, ⟦E3_labeledGraph⟧⟩⟧
 
-/-- a path of length 2 (3 vertices) --/
+/-- a path of length 2 (3 vertices) -/
 noncomputable def P3 : FlagAlgebra ∅ₜ :=
   ⟦unitVector ⟨3, ⟦P3_labeledGraph⟧⟩⟧
 
-/-- a complete graph with 3 vertices --/
+/-- a complete graph with 3 vertices -/
 noncomputable def K3 : FlagAlgebra ∅ₜ :=
   ⟦unitVector ⟨3, ⟦K3_labeledGraph⟧⟩⟧
 
@@ -156,35 +156,35 @@ def K3₁_labeledGraph : LabeledGraph Sₜ (Fin 3) where
       map_rel_iff' := by intros; simp; exact id
     }
 
-/-- a non-edge with one labeled vertex --/
+/-- a non-edge with one labeled vertex -/
 noncomputable def O2₁ : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨2, ⟦O2₁_labeledGraph⟧⟩⟧
 
-/-- an edge with one labeled vertex --/
+/-- an edge with one labeled vertex -/
 noncomputable def K2₁ : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨2, ⟦K2₁_labeledGraph⟧⟩⟧
 
-/-- an empty graph with one labeled vertex --/
+/-- an empty graph with one labeled vertex -/
 noncomputable def O3₁ : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨3, ⟦O3₁_labeledGraph⟧⟩⟧
 
-/-- an edge with one labeled vertex and an isolated vertex --/
+/-- an edge with one labeled vertex and an isolated vertex -/
 noncomputable def E3₁ : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨3, ⟦E3₁_labeledGraph⟧⟩⟧
 
-/-- an edge and an isolated vertex with a label --/
+/-- an edge and an isolated vertex with a label -/
 noncomputable def E3₁' : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨3, ⟦E3₁'_labeledGraph⟧⟩⟧
 
-/-- a path of length 2 (3 vertices) with the middle vertex labeled --/
+/-- a path of length 2 (3 vertices) with the middle vertex labeled -/
 noncomputable def P3₁ : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨3, ⟦P3₁_labeledGraph⟧⟩⟧
 
-/-- a path of length 2 (3 vertices) where one of the endpoints is labeled --/
+/-- a path of length 2 (3 vertices) where one of the endpoints is labeled -/
 noncomputable def P3₁' : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨3, ⟦P3₁'_labeledGraph⟧⟩⟧
 
-/-- a complete graph with one labeled vertex --/
+/-- a complete graph with one labeled vertex -/
 noncomputable def K3₁ : FlagAlgebra Sₜ :=
   ⟦unitVector ⟨3, ⟦K3₁_labeledGraph⟧⟩⟧
 
@@ -221,6 +221,98 @@ lemma downward_O3₁
   dsimp [O3₁, downward, O3, downwardFlagVectorQuot]
   rw [downwardFlagVectorQuot_O3₁]
 
+lemma unlabel_E3₁
+    : unlabel ⟦E3₁_labeledGraph⟧ = ⟦E3_labeledGraph⟧
+  := by
+  sorry
+
+lemma downwardNormalizingFactor_E3₁
+    : downwardNormalizingFactor ⟦E3₁_labeledGraph⟧ = 2 / 3
+  := by
+  sorry
+
+lemma downwardFlagVectorQuot_E3₁
+    : downwardFlagVector (unitVector ⟨3, ⟦E3₁_labeledGraph⟧⟩) = (2 / 3 : ℝ) • unitVector ⟨3, ⟦E3_labeledGraph⟧⟩
+  := by
+  simp [downwardFlagVector, downwardFlag]
+  simp [unlabel_E3₁, downwardNormalizingFactor_E3₁]
+
+lemma downward_E3₁
+    : ⟦E3₁⟧₀ = (2 / 3 : ℝ) • E3
+  := by
+  dsimp [E3₁, downward, E3, downwardFlagVectorQuot]
+  rw [downwardFlagVectorQuot_E3₁]
+  rfl
+
+lemma unlabel_E3₁'
+    : unlabel ⟦E3₁'_labeledGraph⟧ = ⟦E3_labeledGraph⟧
+  := by
+  sorry
+
+lemma downwardNormalizingFactor_E3₁'
+    : downwardNormalizingFactor ⟦E3₁'_labeledGraph⟧ = 1 / 3
+  := by
+  sorry
+
+lemma downwardFlagVectorQuot_E3₁'
+    : downwardFlagVector (unitVector ⟨3, ⟦E3₁'_labeledGraph⟧⟩) = (1 / 3 : ℝ) • unitVector ⟨3, ⟦E3_labeledGraph⟧⟩
+  := by
+  simp [downwardFlagVector, downwardFlag]
+  simp [unlabel_E3₁', downwardNormalizingFactor_E3₁']
+
+lemma downward_E3₁'
+    : ⟦E3₁'⟧₀ = (1 / 3 : ℝ) • E3
+  := by
+  dsimp [E3₁', downward, E3, downwardFlagVectorQuot]
+  rw [downwardFlagVectorQuot_E3₁']
+  rfl
+
+lemma unlabel_P3₁
+    : unlabel ⟦P3₁_labeledGraph⟧ = ⟦P3_labeledGraph⟧
+  := by
+  sorry
+
+lemma downwardNormalizingFactor_P3₁
+    : downwardNormalizingFactor ⟦P3₁_labeledGraph⟧ = 1 / 3
+  := by
+  sorry
+
+lemma downwardFlagVectorQuot_P3₁
+    : downwardFlagVector (unitVector ⟨3, ⟦P3₁_labeledGraph⟧⟩) = (1 / 3 : ℝ) • unitVector ⟨3, ⟦P3_labeledGraph⟧⟩
+  := by
+  simp [downwardFlagVector, downwardFlag]
+  simp [unlabel_P3₁, downwardNormalizingFactor_P3₁]
+
+lemma downward_P3₁
+    : ⟦P3₁⟧₀ = (1 / 3 : ℝ) • P3
+  := by
+  dsimp [P3₁, downward, P3, downwardFlagVectorQuot]
+  rw [downwardFlagVectorQuot_P3₁]
+  rfl
+
+lemma unlabel_P3₁'
+    : unlabel ⟦P3₁'_labeledGraph⟧ = ⟦P3_labeledGraph⟧
+  := by
+  sorry
+
+lemma downwardNormalizingFactor_P3₁'
+    : downwardNormalizingFactor ⟦P3₁'_labeledGraph⟧ = 2 / 3
+  := by
+  sorry
+
+lemma downwardFlagVectorQuot_P3₁'
+    : downwardFlagVector (unitVector ⟨3, ⟦P3₁'_labeledGraph⟧⟩) = (2 / 3 : ℝ) • unitVector ⟨3, ⟦P3_labeledGraph⟧⟩
+  := by
+  simp [downwardFlagVector, downwardFlag]
+  simp [unlabel_P3₁', downwardNormalizingFactor_P3₁']
+
+lemma downward_P3₁'
+    : ⟦P3₁'⟧₀ = (2 / 3 : ℝ) • P3
+  := by
+  dsimp [P3₁', downward, P3, downwardFlagVectorQuot]
+  rw [downwardFlagVectorQuot_P3₁']
+  rfl
+
 lemma unlabel_K3₁
     : unlabel ⟦K3₁_labeledGraph⟧ = ⟦K3_labeledGraph⟧
   := by
@@ -255,6 +347,6 @@ lemma downward_K3₁
 /- proof of Mantel's theorem -/
 
 theorem mantel_theorem
-    : K2 ≤ (1 / 2) • 1 + K3
-  :=
+    : K2 ≤ (1 / 2 : ℝ) • 1 + K3
+  := by
   sorry
