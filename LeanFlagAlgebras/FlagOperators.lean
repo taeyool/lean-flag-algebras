490703b4836b3fa3811f1e22cdd672e1d90c1533
taeyool <jgyw0910@kaist.ac.kr>
2025-05-22 02:07:37 +0900

    Finished proving well-definedness of downwardNormalizingFactor

--- LeanFlagAlgebras/FlagOperators.lean
+++ LeanFlagAlgebras/FlagOperators.lean
@@ -29,71 +29,86 @@ noncomputable def downwardNormalizingFactor_labeledGraph
   let num_of_all_injections := n.factorial / (n - n₀).factorial
   isomorphismCount G / num_of_all_injections
 
+def funBetweenIsoLabeledGraphSetWithSameGraph
+    {G G' : LabeledGraph σ (Fin n)} (φ : G ≃f G')
+    : isoLabeledGraphSetWithSameGraph G → isoLabeledGraphSetWithSameGraph G'
+  := by
+  intro ⟨H, ⟨hGH_graph, hGH_iso⟩⟩
+  let H' : LabeledGraph σ (Fin n) := {
+    graph := G'.graph
+    type_embed := {
+      toFun := φ.graph_iso ∘ H.type_embed
+      inj' := by simp only [EmbeddingLike.comp_injective, RelEmbedding.injective]
+      map_rel_iff' := by
+        intro a b
+        simp_all only [Function.Embedding.coeFn_mk, Function.comp_apply]
+        constructor
+        · intro h
+          rw [type_embed_Adj_iff H]
+          nth_rw 1 [← hGH_graph]
+          exact (SimpleGraph.Iso.map_adj_iff φ.graph_iso).mp h
+        · intro h
+          rw [SimpleGraph.Iso.map_adj_iff φ.graph_iso, hGH_graph, ← type_embed_Adj_iff H]
+          exact h
+    }
+  }
+  have hG'H'_graph : G'.graph = H'.graph := rfl
+  let φH : H.graph ≃g H'.graph := by
+    rw [← hGH_graph, ← hG'H'_graph]
+    exact φ.graph_iso
+  let ψ : G ≃f H := hGH_iso.some
+  have hH' : G'.graph = H'.graph ∧ G' ∼f H' := by
+    constructor
+    · simp
+    · apply Nonempty.intro
+      exact {
+        graph_iso := (φ.graph_iso.symm.trans ψ.graph_iso).trans φH
+        type_preserve := by
+          simp
+          rw [← ψ.type_preserve, ← φ.type_preserve]
+          calc
+            _ = ⇑φH ∘ ⇑ψ.graph_iso ∘ (⇑φ.graph_iso.symm ∘ ⇑φ.graph_iso) ∘ ⇑G.type_embed := rfl
+            _ = ⇑φH ∘ ⇑ψ.graph_iso ∘ ⇑G.type_embed := by ext; simp
+            _ = ⇑φ.graph_iso ∘ ⇑ψ.graph_iso ∘ ⇑G.type_embed := by
+              have : ⇑φH = ⇑φ.graph_iso := by
+                show φH.toFun = φ.graph_iso
+                dsimp [φH]
+                funext x
+                congr
+                · rw [hGH_graph]
+                · rw [hGH_graph]
+                · simp [cast_heq]
+              rw [this]
+      }
+  exact ⟨H', hH'⟩
+
+lemma comp_funBetweenIsoLabeledGraphSetWithSameGraph
+    {G G' : LabeledGraph σ (Fin n)} (φ : G ≃f G')
+    : ∀ H, (funBetweenIsoLabeledGraphSetWithSameGraph φ.symm) ((funBetweenIsoLabeledGraphSetWithSameGraph φ) H) = H
+  := by
+  intro ⟨H, ⟨hGH_graph, hGH_iso⟩⟩
+  unfold funBetweenIsoLabeledGraphSetWithSameGraph
+  split
+  rename_i h H_1 hGH_graph_1 hGH_iso_1 heq
+  simp_all only [Subtype.mk.injEq]
+  subst heq
+  simp_all only [RelEmbedding.coe_mk, Function.Embedding.coeFn_mk]
+  congr
+  · simp_all only
+  · simp_all only [Function.Embedding.toFun_eq_coe, RelEmbedding.coe_toEmbedding]
+    calc
+      _ = (⇑φ.graph_iso.symm ∘ ⇑φ.graph_iso) ∘ ⇑H.type_embed := rfl
+      _ = ⇑H.type_embed := by ext; simp
+      _ = ⇑H.2 := rfl
+  · apply proof_irrel_heq
+
 def isoSetOfIsoLabeledGraphWithSameGraph
     {G G' : LabeledGraph σ (Fin n)} (φ : G ≃f G')
     : isoLabeledGraphSetWithSameGraph G ≃ isoLabeledGraphSetWithSameGraph G' where
-  toFun := by
-    intro ⟨H, ⟨hGH_graph, hGH_iso⟩⟩
-    let H' : LabeledGraph σ (Fin n) := {
-      graph := G'.graph
-      type_embed := {
-        toFun := φ.graph_iso ∘ H.type_embed
-        inj' := by simp only [EmbeddingLike.comp_injective, RelEmbedding.injective]
-        map_rel_iff' := by
-          intro a b
-          simp_all only [Function.Embedding.coeFn_mk, Function.comp_apply]
-          constructor
-          · intro h
-            rw [type_embed_Adj_iff H]
-            nth_rw 1 [← hGH_graph]
-            exact (SimpleGraph.Iso.map_adj_iff φ.graph_iso).mp h
-          · intro h
-            rw [SimpleGraph.Iso.map_adj_iff φ.graph_iso, hGH_graph, ← type_embed_Adj_iff H]
-            exact h
-      }
-    }
-    have hG'H'_graph : G'.graph = H'.graph := rfl
-    -- let φH : H.graph ≃g H'.graph := by
-    --   rw [← hGH_graph, ← hG'H'_graph]
-    --   exact φ.graph_iso
-    let φn : Fin n ≃ Fin n := φ.graph_iso
-    let φH : H.graph ≃g H'.graph := {
-      toFun := φn
-      invFun := φn.symm
-      left_inv := φn.left_inv
-      right_inv := φn.right_inv
-      map_rel_iff' := by
-        intro a b
-        simp
-        rw [← hGH_graph]
-        sorry
-    }
-    have : φH.toFun = φn := by aesop
-    have : ⇑φH = ⇑φ.graph_iso := by
-      simp_all only [Function.Embedding.coeFn_mk, Function.comp_apply, id_eq, eq_mpr_eq_cast, cast_eq, H', φH]
-      sorry
-    let ψ : G ≃f H := hGH_iso.some
-    have hH' : G'.graph = H'.graph ∧ G' ∼f H' := by
-      constructor
-      · simp
-      · apply Nonempty.intro
-        exact {
-          graph_iso := (φ.graph_iso.symm.trans ψ.graph_iso).trans φH
-          type_preserve := by
-            simp
-            rw [← ψ.type_preserve, ← φ.type_preserve]
-            calc
-              _ = ⇑φH ∘ ⇑ψ.graph_iso ∘ (⇑φ.graph_iso.symm ∘ ⇑φ.graph_iso) ∘ ⇑G.type_embed := rfl
-              _ = ⇑φH ∘ ⇑ψ.graph_iso ∘ ⇑G.type_embed := by ext; simp
-              _ = ⇑φ.graph_iso ∘ ⇑ψ.graph_iso ∘ ⇑G.type_embed := by
-                ext x
-                simp
-                sorry
-        }
-    exact ⟨H', hH'⟩
-  invFun := sorry
-  left_inv := sorry
-  right_inv := sorry
+  toFun := funBetweenIsoLabeledGraphSetWithSameGraph φ
+  invFun := funBetweenIsoLabeledGraphSetWithSameGraph φ.symm
+  left_inv := comp_funBetweenIsoLabeledGraphSetWithSameGraph φ
+  right_inv := comp_funBetweenIsoLabeledGraphSetWithSameGraph φ.symm
 
 lemma isomorphismCount_respects_eqv
     {G G' : LabeledGraph σ (Fin n)} (h : G ∼f G')
