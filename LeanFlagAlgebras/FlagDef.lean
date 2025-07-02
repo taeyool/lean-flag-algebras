7b5c7520bbd22d67c04b6b846a1fbf578d67cde4
Hongseok Yang <hongseok00@gmail.com>
2025-07-02 16:23:52 +0900

    minor simplification

--- LeanFlagAlgebras/FlagDef.lean
+++ LeanFlagAlgebras/FlagDef.lean
@@ -146,7 +146,7 @@ def LabeledGraph.bottom (G : LabeledGraph σ V) : LabeledSubgraph σ G :=
   }
 
 lemma LabeledGraph.bottom_isInduced (G : LabeledGraph σ V)
-  : G.bottom.subgraph.IsInduced := fun h_u h_v h_adj ↦ ⟨h_u, ⟨h_v, h_adj⟩⟩
+  : G.bottom.subgraph.IsInduced := fun h_u h_v h_adj ↦ ⟨h_u, h_v, h_adj⟩
 
 namespace LabeledSubgraph
 
