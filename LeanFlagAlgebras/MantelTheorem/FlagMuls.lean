ba8355d4e11ad3e81be70f63bf8eead0f815076c
taeyool <jgyw0910@kaist.ac.kr>
2025-07-16 16:29:48 +0900

    Minor bugfix

--- LeanFlagAlgebras/MantelTheorem/FlagMuls.lean
+++ LeanFlagAlgebras/MantelTheorem/FlagMuls.lean
@@ -8,8 +8,8 @@ namespace MantelTheorem
 theorem mul_O2₁_O2₁
     : O2₁ * O2₁ = O3₁ + E3₁'
   := by
-  apply Quotient.sound; simp
-  simp [flagVector_mul_def, flagMul, flagMulWithSize]
+  apply Quotient.sound
+  simp [flagVector_mul_eq_nested_sum, flagMul, flagMulWithSize]
   rw [Finset.sum_eq_multiset_sum, ← singletonTypeThreeVertexFlagSet_eq_univ]
   simp [singletonTypeThreeVertexFlagSet]
   rfl
@@ -17,8 +17,8 @@ theorem mul_O2₁_O2₁
 theorem mul_O2₁_K2₁
     : O2₁ * K2₁ = (1 / 2 : ℝ) • E3₁ + (1 / 2 : ℝ) • P3₁'
   := by
-  apply Quotient.sound; simp
-  simp [flagVector_mul_def, flagMul, flagMulWithSize]
+  apply Quotient.sound
+  simp [flagVector_mul_eq_nested_sum, flagMul, flagMulWithSize]
   rw [Finset.sum_eq_multiset_sum, ← singletonTypeThreeVertexFlagSet_eq_univ]
   simp [singletonTypeThreeVertexFlagSet]
   rfl
@@ -26,8 +26,8 @@ theorem mul_O2₁_K2₁
 theorem mul_K2₁_K2₁
     : K2₁ * K2₁ = P3₁ + K3₁
   := by
-  apply Quotient.sound; simp
-  simp [flagVector_mul_def, flagMul, flagMulWithSize]
+  apply Quotient.sound
+  simp [flagVector_mul_eq_nested_sum, flagMul, flagMulWithSize]
   rw [Finset.sum_eq_multiset_sum, ← singletonTypeThreeVertexFlagSet_eq_univ]
   simp [singletonTypeThreeVertexFlagSet]
   rfl
