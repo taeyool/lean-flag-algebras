6f70ce93c66d5a874c4e36229e3b3deed4d8e0c2
taeyool <jgyw0910@kaist.ac.kr>
2025-07-23 15:31:15 +0900

    Trying to prove Theorem 3.2 from Razborov's paper (every increasing flag sequence contains a convergent subsequence)

--- LeanFlagAlgebras/FlagSequence.lean
+++ LeanFlagAlgebras/FlagSequence.lean
@@ -13,6 +13,10 @@ abbrev FlagSeq (σ : FlagType (Fin n₀))
   :=
   ℕ → FinFlag σ
 
+noncomputable def flagDensitySeq (F : FinFlag σ) (s : FlagSeq σ) : ℕ → ℝ
+  :=
+  fun n => (flagDensity₁ F.2 (s n).2 : ℝ)
+
 def Increases (s : FlagSeq σ) : Prop
   :=
   StrictMono (fun n => (s n).1)
@@ -20,9 +24,31 @@ def Increases (s : FlagSeq σ) : Prop
 def ConvergesTo (s : FlagSeq σ) (a : FinFlag σ → ℝ) : Prop
   :=
   Increases s ∧
-  ∀ (F : FinFlag σ), Tendsto (fun n => (flagDensity₁ F.2 (s n).2 : ℝ)) atTop (𝓝 (a F))
+  ∀ (F : FinFlag σ), Tendsto (flagDensitySeq F s) atTop (𝓝 (a F))
 
-#check CompactSpace.tendsto_subseq
+def flagDensitySpace (σ : FlagType (Fin n₀))
+  :=
+  Set.pi (Set.univ : Set (FinFlag σ)) (fun _ => (Set.Icc 0 1 : Set ℝ))
+
+instance : Countable (FinFlag σ)
+  := by
+  sorry
+
+theorem flagDensitySpace_compact
+    : IsCompact (flagDensitySpace σ)
+  := by
+  dsimp [flagDensitySpace, Set.pi]
+  simp only [Set.mem_univ, forall_true_left]
+  apply isCompact_pi_infinite
+  intro _
+  exact isCompact_Icc
+
+instance : CompactSpace (flagDensitySpace σ)
+  :=
+  isCompact_iff_compactSpace.mp flagDensitySpace_compact
+
+#check isCompact_pi_infinite
+#check @CompactSpace.tendsto_subseq (flagDensitySpace σ) _ _ _
 
 theorem increasing_flagSeq_contain_convergent_subseq
     (s : FlagSeq σ) (hs_inc : Increases s)
