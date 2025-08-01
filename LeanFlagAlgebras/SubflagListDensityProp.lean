18e4bf4f4e05d5852a59b43837985563dff49cc8
rhaxod22 <hun57@kaist.ac.kr>
2025-08-01 14:26:38 +0900

    minor

--- LeanFlagAlgebras/SubflagListDensityProp.lean
+++ LeanFlagAlgebras/SubflagListDensityProp.lean
@@ -21,90 +21,6 @@ variable {σ : FlagType T} {t : ℕ}
 variable {Vl  : Fin t → Type} [FintypeList Vl]  [DecidableEqList Vl]
 variable {Fl : FlagList σ t Vl}
 
--- Version 1. define the single space, then use the product space
-
-def SingleChoiceSpace
-  (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Type _ :=
-  { S : Finset V // S.card = (Fl i).out.size ∧ G.type_verts ⊆ S.toSet }
-instance (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Fintype (SingleChoiceSpace Fl G i) := by
-  apply Subtype.fintype
-instance (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : MeasurableSpace (SingleChoiceSpace Fl G i) :=
-  sorry
-instance (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Nonempty (SingleChoiceSpace Fl G i) := by
-  dsimp [SingleChoiceSpace]
-  let S := (Fl i).out.top.subgraph.verts.toFinset
-  sorry
-
-def P_single (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Measure (SingleChoiceSpace Fl G i) :=
-  (PMF.uniformOfFintype (SingleChoiceSpace Fl G i)).toMeasure
-instance P_single.isProbabilityMeasure (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) :
-  IsProbabilityMeasure (P_single Fl G i) := by
-  apply PMF.toMeasure.isProbabilityMeasure
-
-def Ω (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) : Type _ := (i : Fin t) → SingleChoiceSpace Fl G i
-instance measurableSpaceΩ (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) : MeasurableSpace (Ω Fl G) :=
-  MeasurableSpace.pi
-
-def P (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) : Measure (Ω Fl G) :=
-  Measure.pi (fun i => P_single Fl G i)
-instance P.isProbabilityMeasure (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) :
-  IsProbabilityMeasure (P Fl G) := sorry
-
--- Version 2. define the product space directly
-
-def base_verts (G : LabeledGraph σ V) : Finset V := Finset.univ \ G.type_verts.toFinset
-def r_list (Fl : LabeledGraphList σ t Vl) : Fin t → ℕ := fun i => (Fl i).size - σ.size
-
-def SampleSpace (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : Type _ :=
-  { parts : Fin t → Finset V // ∀ i, parts i ⊆ base_verts G ∧ ∀ i, (parts i).card = (r_list Fl) i ∧ ∀ i j, i ≠ j → Disjoint (parts i) (parts j)}
-instance SampleSpace.fintype (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : Fintype (SampleSpace Fl G) := by
-  apply Subtype.fintype
-instance SampleSpace.measurableSpace (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : MeasurableSpace (SampleSpace Fl G) :=
-  sorry
-instance SampleSpace.nonempty (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : Nonempty (SampleSpace Fl G) := by
-  sorry
-
-theorem SampleSpace_eq_multinomialCoefficient
-    (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V)
-    : Fintype.card (SampleSpace Fl G) = multinomialCoefficient (r_list Fl) (G.size - σ.size):= by
-  sorry
-
-/- Lemma 2.3 -/
-
-theorem flagListDensity_prod_approx
-    (Fl : FlagList σ t Vl) (G : Flag σ W)
-    : ∃ k , abs (flagListDensity Fl G - ∏ i in Finset.univ, flagDensity₁ (Fl i) G ) ≤ (∑ i in Finset.univ, (Fl i).out.size ) ^ k / G.out.size
-  := by
-  use 2
-  let Vs := Fin t → Finset W
-  let Ω : Finset Vs := { Vs : Vs | ∀ i , (Vs i).card = (Fl i).out.size ∧ ∀ i, G.out.type_verts ⊆ (Vs i).toSet }
-  let B : Finset Vs := { Vs : Vs | ∀ i j, i ≠ j → Disjoint (Vs i) (Vs j) }
-  let B_c : Finset Vs := { Vs : Vs | ¬(∀ i j, i ≠ j → Disjoint (Vs i) (Vs j)) }
-  let B_c_ij : Fin t → Fin t → Finset Vs := fun i j => { Vs : Vs | ¬ Disjoint (Vs i) (Vs j) }
-  have : B_c.card ≤ ∑ i : Fin t, ∑ j : Fin t, (B_c_ij i j).card := sorry
-  sorry
-
-theorem flagListDensity_prod_approx'
-    (F : Flag σ U) (G : Flag σ W)
-    : ∃ k , abs (flagDensity₂ F F G - flagDensity₁ F G * flagDensity₁ F G ) ≤ (2 * F.out.size) ^ k / G.out.size
-  := by
-  use 2
-  let ⟨Frep, hFrep⟩ := Quotient.exists_rep F
-  let ⟨Grep, hGrep⟩ := Quotient.exists_rep G
-  dsimp [flagDensity₁]
-  rw [← subflagDensity_eq_flagListDensity F G]
-  rw [← hFrep, ← hGrep]
-  rw [← labeledSubgraphListDensity_eq_flagDensity₂ Frep Frep Grep]
-  dsimp [subflagDensity, labeledSubgraphDensityLifted]
-  have hFrep' : ⟦Frep⟧.out.size = Frep.size := rfl
-  have hGrep' : ⟦Grep⟧.out.size = Grep.size := rfl
-  rw [hFrep', hGrep']
-  dsimp [labeledSubgraphDensity, labeledSubgraphListDensity] -- Is it possible to prove using cardinality....?
-
-  let Ω := { v : Finset W × Finset W // v.1.card = Frep.size ∧ v.2.card = Frep.size}
-  sorry
-
-
 def partitions [Fintype α] [DecidableEq α] (V : Finset α) (r_list : Fin t → ℕ) : Finset (Fin t → Finset α)
   := (Finset.univ : Finset (Fin t → Finset α)).filter (fun p =>
       (∀ i, p i ⊆ V ∧ (p i).card = r_list i) ∧
@@ -268,3 +184,86 @@ theorem flagListDensity₁_le_one
     (F : Flag σ V) (G : Flag σ W)
     : flagDensity₁ F G ≤ 1 := by
   apply flagListDensity_le_one
+
+-- Version 1. define the single space, then use the product space
+
+def SingleChoiceSpace
+  (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Type _ :=
+  { S : Finset V // S.card = (Fl i).out.size ∧ G.type_verts ⊆ S.toSet }
+instance (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Fintype (SingleChoiceSpace Fl G i) := by
+  apply Subtype.fintype
+instance (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : MeasurableSpace (SingleChoiceSpace Fl G i) :=
+  sorry
+instance (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Nonempty (SingleChoiceSpace Fl G i) := by
+  dsimp [SingleChoiceSpace]
+  let S := (Fl i).out.top.subgraph.verts.toFinset
+  sorry
+
+def P_single (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) : Measure (SingleChoiceSpace Fl G i) :=
+  (PMF.uniformOfFintype (SingleChoiceSpace Fl G i)).toMeasure
+instance P_single.isProbabilityMeasure (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) (i : Fin t) :
+  IsProbabilityMeasure (P_single Fl G i) := by
+  apply PMF.toMeasure.isProbabilityMeasure
+
+def Ω (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) : Type _ := (i : Fin t) → SingleChoiceSpace Fl G i
+instance measurableSpaceΩ (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) : MeasurableSpace (Ω Fl G) :=
+  MeasurableSpace.pi
+
+def P (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) : Measure (Ω Fl G) :=
+  Measure.pi (fun i => P_single Fl G i)
+instance P.isProbabilityMeasure (Fl : FlagList σ t Vl) (G : LabeledGraph σ V) :
+  IsProbabilityMeasure (P Fl G) := sorry
+
+-- Version 2. define the product space directly
+
+def base_verts (G : LabeledGraph σ V) : Finset V := Finset.univ \ G.type_verts.toFinset
+def r_list (Fl : LabeledGraphList σ t Vl) : Fin t → ℕ := fun i => (Fl i).size - σ.size
+
+def SampleSpace (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : Type _ :=
+  { parts : Fin t → Finset V // ∀ i, parts i ⊆ base_verts G ∧ ∀ i, (parts i).card = (r_list Fl) i ∧ ∀ i j, i ≠ j → Disjoint (parts i) (parts j)}
+instance SampleSpace.fintype (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : Fintype (SampleSpace Fl G) := by
+  apply Subtype.fintype
+instance SampleSpace.measurableSpace (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : MeasurableSpace (SampleSpace Fl G) :=
+  sorry
+instance SampleSpace.nonempty (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V) : Nonempty (SampleSpace Fl G) := by
+  sorry
+
+theorem SampleSpace_eq_multinomialCoefficient
+    (Fl : LabeledGraphList σ t Vl) (G : LabeledGraph σ V)
+    : Fintype.card (SampleSpace Fl G) = multinomialCoefficient (r_list Fl) (G.size - σ.size):= by
+  sorry
+
+/- Lemma 2.3 -/
+
+theorem flagListDensity_prod_approx
+    (Fl : FlagList σ t Vl) (G : Flag σ W)
+    : ∃ k , abs (flagListDensity Fl G - ∏ i in Finset.univ, flagDensity₁ (Fl i) G ) ≤ (∑ i in Finset.univ, (Fl i).out.size ) ^ k / G.out.size
+  := by
+  use 2
+  let Vs := Fin t → Finset W
+  let Ω : Finset Vs := { Vs : Vs | ∀ i , (Vs i).card = (Fl i).out.size ∧ ∀ i, G.out.type_verts ⊆ (Vs i).toSet }
+  let B : Finset Vs := { Vs : Vs | ∀ i j, i ≠ j → Disjoint (Vs i) (Vs j) }
+  let B_c : Finset Vs := { Vs : Vs | ¬(∀ i j, i ≠ j → Disjoint (Vs i) (Vs j)) }
+  let B_c_ij : Fin t → Fin t → Finset Vs := fun i j => { Vs : Vs | ¬ Disjoint (Vs i) (Vs j) }
+  have : B_c.card ≤ ∑ i : Fin t, ∑ j : Fin t, (B_c_ij i j).card := sorry
+  sorry
+
+theorem flagListDensity_prod_approx'
+    (F : Flag σ U) (G : Flag σ W)
+    : ∃ k , abs (flagDensity₂ F F G - flagDensity₁ F G * flagDensity₁ F G ) ≤ (2 * F.out.size) ^ k / G.out.size
+  := by
+  use 2
+  let ⟨Frep, hFrep⟩ := Quotient.exists_rep F
+  let ⟨Grep, hGrep⟩ := Quotient.exists_rep G
+  dsimp [flagDensity₁]
+  rw [← subflagDensity_eq_flagListDensity F G]
+  rw [← hFrep, ← hGrep]
+  rw [← labeledSubgraphListDensity_eq_flagDensity₂ Frep Frep Grep]
+  dsimp [subflagDensity, labeledSubgraphDensityLifted]
+  have hFrep' : ⟦Frep⟧.out.size = Frep.size := rfl
+  have hGrep' : ⟦Grep⟧.out.size = Grep.size := rfl
+  rw [hFrep', hGrep']
+  dsimp [labeledSubgraphDensity, labeledSubgraphListDensity] -- Is it possible to prove using cardinality....?
+
+  let Ω := { v : Finset W × Finset W // v.1.card = Frep.size ∧ v.2.card = Frep.size}
+  sorry
