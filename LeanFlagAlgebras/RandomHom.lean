4c8966d92ac1262d8e080b2162510191ece77f4e
taeyool <jgyw0910@kaist.ac.kr>
2025-08-24 15:47:23 +0900

    Minor

--- LeanFlagAlgebras/RandomHom.lean
+++ LeanFlagAlgebras/RandomHom.lean
@@ -32,8 +32,7 @@ theorem flagDensity₁_flagType_asEmptyType
   dsimp only [flagDensity₁]
   rw [← subflagDensity_eq_flagListDensity]
   dsimp only [flagType_asEmptyTypeFlag, unlabel, subflagDensity, Quotient.lift_mk]
-  dsimp only [labeledSubgraphDensityLifted, labeledSubgraphDensity,
-    Quotient.lift_mk]
+  dsimp only [labeledSubgraphDensityLifted, labeledSubgraphDensity, Quotient.lift_mk]
   apply div_pos
   · simp only [Nat.cast_pos, labeledSubgraphCount]
     rw [Finset.card_pos]
