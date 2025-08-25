650d1fc2e966d4b2ab0c00dcd910a01677f66a4a
rhaxod22 <hun57@kaist.ac.kr>
2025-08-25 17:38:25 +0900

    Some progress

--- LeanFlagAlgebras/SubflagListDensityProp.lean
+++ LeanFlagAlgebras/SubflagListDensityProp.lean
@@ -27,7 +27,6 @@ variable {σ : FlagType T} {t : ℕ}
 variable {Vl  : Fin t → Type} [FintypeList Vl]  [DecidableEqList Vl]
 variable {Fl : FlagList σ t Vl}
 
-
 -- Version 1. define the single space, then use the product space
 
 def SingleChoiceSpace
@@ -99,6 +98,9 @@ theorem flagListDensity_prod_approx
   have : B_c.card ≤ ∑ i : Fin t, ∑ j : Fin t, (B_c_ij i j).card := sorry
   sorry
 
+example (a b : ℕ) : (a + b) ^ 2 = a ^ 2 + b ^ 2 + 2 * (a * b) := by
+  ring
+
 theorem flagListDensity₂_prod_approx
     (F : Flag σ V) (F' : Flag σ U)
     : ∃ c ≥ 0, ∀ {W : Type} [Fintype W] [DecidableEq W] (G : Flag σ W),
@@ -107,18 +109,33 @@ theorem flagListDensity₂_prod_approx
   use (F.out.size + F'.out.size) ^ 2
   constructor; (apply sq_nonneg)
   intro W _ _ G
-  let ⟨Frep, hFrep⟩ := Quotient.exists_rep F
-  let ⟨F'rep, hF'rep⟩ := Quotient.exists_rep F'
-  let ⟨Grep, hGrep⟩ := Quotient.exists_rep G
+  let ⟨Frep, hFrep₁⟩ := Quotient.exists_rep F
+  let ⟨F'rep, hF'rep₁⟩ := Quotient.exists_rep F'
+  let ⟨Grep, hGrep₁⟩ := Quotient.exists_rep G
   dsimp [flagDensity₁]
-  rw [← subflagDensity_eq_flagListDensity F G]
-  rw [← hFrep, ← hF'rep, ← hGrep]
+  rw [← subflagDensity_eq_flagListDensity F G, ← subflagDensity_eq_flagListDensity F' G]
+  rw [← hFrep₁, ← hF'rep₁, ← hGrep₁]
   rw [← labeledSubgraphListDensity_eq_flagDensity₂ Frep F'rep Grep]
   dsimp [subflagDensity, labeledSubgraphDensityLifted]
-  have hFrep' : (⟦Frep⟧ : Quotient (labeledGraphSetoid σ V)).out.size = Frep.size := rfl
-  have hGrep' : (⟦Grep⟧ : Quotient (labeledGraphSetoid σ W)).out.size = Grep.size := rfl
-  rw [hFrep', hGrep']
-  dsimp [labeledSubgraphDensity, labeledSubgraphListDensity] -- Is it possible to prove using cardinality....?
+  have hFrep₂ : (⟦Frep⟧ : Quotient (labeledGraphSetoid σ V)).out.size = Frep.size := rfl
+  have hF'rep₂ : (⟦F'rep⟧ : Quotient (labeledGraphSetoid σ U)).out.size = F'rep.size := rfl
+  have hGrep₂ : (⟦Grep⟧ : Quotient (labeledGraphSetoid σ W)).out.size = Grep.size := rfl
+  rw [hFrep₂, hF'rep₂, hGrep₂]
 
-  let Ω := { v : Finset W × Finset W // v.1.card = Frep.size ∧ v.2.card = Frep.size}
-  sorry
+  let Ω := (Finset.univ : Finset (Finset W × Finset W)).filter (fun v => (v.1.card = Frep.size ∧ Grep.type_verts ⊆ v.1) ∧ (v.2.card = F'rep.size ∧ Grep.type_verts ⊆ v.2))
+  let A := Ω.filter (fun v => Nonempty ((inducedLabeledSubgraph Grep v.1.toSet (by sorry)).coe ≃f Frep) ∧ Nonempty ((inducedLabeledSubgraph Grep v.2.toSet (by sorry)).coe ≃f F'rep))
+  let B := Ω.filter (fun (v : Finset W × Finset W) => (v.1 \ Grep.type_verts.toFinset) ∩ (v.2 \ Grep.type_verts.toFinset) = ∅)
+
+  have calc₁ : 2 * Frep.size * F'rep.size ≤ (Frep.size + F'rep.size) ^ 2 := by
+    ring_nf
+    apply Nat.le_add_right_of_le (Nat.le_add_right_of_le (le_refl _))
+  rw [← Nat.cast_le (α := ℚ)] at calc₁
+  have calc₂ : ((2 : ℚ) * ↑Frep.size * ↑F'rep.size) / ↑Grep.size ≤ ((↑Frep.size + ↑F'rep.size) ^ 2) / ↑Grep.size := by
+    refine (div_le_div_iff_of_pos_right ?_).mpr ?_
+    · sorry
+    · simp only [Nat.cast_mul, Nat.cast_ofNat, Nat.cast_pow, Nat.cast_add] at calc₁
+      exact calc₁
+  have calc₃ :  |labeledSubgraphListDensity (labeledGraphPairToList Frep F'rep) Grep -
+      labeledSubgraphDensity Frep Grep * labeledSubgraphDensity F'rep Grep| ≤ ((2 : ℚ) * ↑Frep.size * ↑F'rep.size) / ↑Grep.size := by
+    sorry
+  exact calc₃.trans calc₂
