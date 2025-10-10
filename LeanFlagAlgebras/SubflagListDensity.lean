358ab5b2f11e29a662b46c61e5781fefb9a1b75e
Hongseok Yang <hongseok00@gmail.com>
2025-10-10 17:49:19 +0900

    in the middle of proving surjectivity

--- LeanFlagAlgebras/SubflagListDensity.lean
+++ LeanFlagAlgebras/SubflagListDensity.lean
@@ -2332,8 +2332,6 @@ noncomputable def
         | 1 => Subtype.val '' (iso₀.graph_iso '' (Vl' 1))
         | 2 => Vl'' 1
 
-      have h_V_card : V.toFinset.card = ℓ'_other := by sorry
-
       have h_V_disj_Vl : ∀ i : Fin 3, V ∩ (Vl i) = ∅ := by
         dsimp [V, Vl]
         intro i
@@ -2369,12 +2367,35 @@ noncomputable def
               (Vl'' 0 \ (Subtype.val '' (⇑iso₀.graph_iso '' Vl' 0) ∪ Subtype.val '' (⇑iso₀.graph_iso '' Vl' 1) ∪ G.type_verts)) ∩ Vl'' 1
               _ ⊆ (Vl'' 0) ∩ (Vl'' 1) := Set.inter_subset_inter_left _ Set.diff_subset
               _ = ∅ := disjoint_iff.mp (h_Vl''_disj_pairwise (Set.mem_univ 0) (Set.mem_univ 1) (by omega))
+      have h_V_disj_G_type_verts : V ∩ G.type_verts = ∅ := by
+        dsimp [V]
+        apply Set.eq_empty_of_subset_empty
+        calc
+          (Vl'' 0 \ (Subtype.val '' (⇑iso₀.graph_iso '' Vl' 0) ∪ Subtype.val '' (⇑iso₀.graph_iso '' Vl' 1) ∪ G.type_verts)) ∩ G.type_verts
+          _ ⊆
+          (Vl'' 0 \ (Subtype.val '' (⇑iso₀.graph_iso '' Vl' 0) ∪ Subtype.val '' (⇑iso₀.graph_iso '' Vl' 1) ∪ G.type_verts))
+              ∩ (Subtype.val '' (⇑iso₀.graph_iso '' Vl' 0) ∪ Subtype.val '' (⇑iso₀.graph_iso '' Vl' 1) ∪ G.type_verts)
+                := Set.inter_subset_inter_right _ Set.subset_union_right
+          _ = ∅ := Set.diff_inter_self
+      have h_Vl_disj_G_type_verts : ∀ i : Fin 3, (Vl i) ∩ G.type_verts = ∅ := by
+        intro i
+        dsimp [Vl]
+        match i with
+        | 0 =>
+            simp only [Fin.isValue]
+            sorry
+        | 1 =>
+            simp only [Fin.isValue]
+            sorry
+        | 2 =>
+            simp only [Fin.isValue]
+            exact h_Vl''_disj_type_verts 1
 
-      have h_V_disj_G_type_verts : V ∩ G.type_verts = ∅ := by sorry
+      have h_Vl_disj_pairwise : Set.univ.PairwiseDisjoint Vl := by sorry
+
+      have h_V_card : V.toFinset.card = ℓ'_other := by sorry
       have h_Vl_card : ∀ i : Fin 3, (Vl i).toFinset.card = Hl_size i - ℓ₀ := by sorry
       have h_Vl_iso : ∀ i : Fin 3, Nonempty ((inducedLabeledSubgraph G ((Vl i) ∪ G.type_verts) Set.subset_union_right).coe ≃f (Hl i)) := by sorry
-      have h_Vl_disj_G_type_verts : ∀ i : Fin 3, (Vl i) ∩ G.type_verts = ∅ := by sorry
-      have h_Vl_disj_pairwise : Set.univ.PairwiseDisjoint Vl := by sorry
 
       use ⟨⟨V, Vl⟩,
         (by rw [←h_V_card]; congr!), h_V_disj_Vl, h_V_disj_G_type_verts,
