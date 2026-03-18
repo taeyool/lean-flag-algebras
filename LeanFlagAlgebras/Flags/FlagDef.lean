ad7bca613531b236ba19c87e15bdae78fe2adece
rhaxod22 <hun57@kaist.ac.kr>
2026-03-18 16:09:17 +0900

    Minor fix at FlagLoader.lean

--- LeanFlagAlgebras/Flags/FlagDef.lean
+++ LeanFlagAlgebras/Flags/FlagDef.lean
@@ -4,10 +4,15 @@ load_empty_typed_flags "LeanFlagAlgebras/Flags/Graphs/graphs_0.json"
 load_empty_typed_flags "LeanFlagAlgebras/Flags/Graphs/graphs_1.json"
 load_empty_typed_flags "LeanFlagAlgebras/Flags/Graphs/graphs_2.json"
 load_empty_typed_flags "LeanFlagAlgebras/Flags/Graphs/graphs_3.json"
+load_empty_typed_flags "LeanFlagAlgebras/Flags/Graphs/graphs_4.json"
+load_empty_typed_flags "LeanFlagAlgebras/Flags/Graphs/graphs_5.json"
 
 load_flags "LeanFlagAlgebras/Flags/Flags/flags_1_1_0.json"
 load_flags "LeanFlagAlgebras/Flags/Flags/flags_2_1_0.json"
 load_flags "LeanFlagAlgebras/Flags/Flags/flags_3_1_0.json"
+
+load_flags "LeanFlagAlgebras/Flags/Flags/flags_4_3_2.json"
+load_flags "LeanFlagAlgebras/Flags/Flags/flags_5_3_2.json"
 
 #print Sym2LabeledGraph_3_1_0_2
 #check downward_3_1_0_2
