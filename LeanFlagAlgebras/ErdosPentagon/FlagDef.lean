4a1d825029ffe5147e0715f7c0110f6c83d33fb2
rhaxod22 <hun57@kaist.ac.kr>
2026-03-26 17:52:30 +0900

    Minor

--- LeanFlagAlgebras/ErdosPentagon/FlagDef.lean
+++ LeanFlagAlgebras/ErdosPentagon/FlagDef.lean
@@ -1,4 +1,8 @@
 import LeanFlagAlgebras.Flags.FlagDef
+import LeanFlagAlgebras.FlagAlgebra.QuadraticForm
+import LeanFlagAlgebras.ErdosPentagon.Matrix.PosSemiDef
+
+open FlagAlgebras
 
 namespace ErdosPentagon
 
@@ -6,5 +10,11 @@ noncomputable abbrev K3 := FlagAlgebra_3_0_0_3
 noncomputable abbrev C5 := FlagAlgebra_5_0_0_19
 
 #print Sym2Graph_5_0_0_19 -- s(0, 1), s(0, 2), s(1, 3), s(2, 4), s(3, 4)
+
+def σ₂ : FlagType (Fin 3) := FlagType_3_2
+
+noncomputable def Vᵣ : FlagAlgebraVec σ₂ 5 := ![
+  FlagAlgebra_4_3_2_0, FlagAlgebra_4_3_2_1, FlagAlgebra_4_3_2_2, FlagAlgebra_4_3_2_3, FlagAlgebra_4_3_2_6
+]
 
 end ErdosPentagon
