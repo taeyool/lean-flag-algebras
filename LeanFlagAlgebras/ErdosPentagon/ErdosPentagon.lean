afcbb105d0a2fc51ef397f9be52192c2e2e77e8e
taeyool <jgyw0910@kaist.ac.kr>
2026-04-06 17:08:53 +0900

    Added load_mul_relations command to automatically generate flag multiplication theorems

--- LeanFlagAlgebras/ErdosPentagon/ErdosPentagon.lean
+++ LeanFlagAlgebras/ErdosPentagon/ErdosPentagon.lean
@@ -9,6 +9,11 @@ example
   :=
   flagQuadraticForm_nonneg R_real R_real_posSemidef Vᵣ
 
+example
+    : flagQuadraticForm R_real Vᵣ =[K3] sorry
+  := by
+  sorry
+
 theorem ErdosPentagon
     : C5 ≤[K3] (24 / 625 : ℝ) • (1 : FlagAlgebra ∅ₜ)
   := by
