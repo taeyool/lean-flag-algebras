c6b27efad00081929d53d5e2a185949503ba7b0c
taeyool <jgyw0910@kaist.ac.kr>
2026-04-06 16:21:35 +0900

    Minor

---
+++ LeanFlagAlgebras/ErdosPentagon/ErdosPentagon.lean
@@ -0,0 +1,17 @@
+import LeanFlagAlgebras.ErdosPentagon.FlagDensity
+
+open FlagAlgebras
+
+namespace ErdosPentagon
+
+example
+    : flagQuadraticForm R_real Vᵣ ≥ 0
+  :=
+  flagQuadraticForm_nonneg R_real R_real_posSemidef Vᵣ
+
+theorem ErdosPentagon
+    : C5 ≤[K3] (24 / 625 : ℝ) • (1 : FlagAlgebra ∅ₜ)
+  := by
+  sorry
+
+end ErdosPentagon
