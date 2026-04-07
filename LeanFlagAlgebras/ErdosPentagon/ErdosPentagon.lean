4af0fa7334e58dba1c57e3bcf63907f9e3c5755a
taeyool <jgyw0910@kaist.ac.kr>
2026-04-07 17:37:17 +0900

    Minor

--- LeanFlagAlgebras/ErdosPentagon/ErdosPentagon.lean
+++ LeanFlagAlgebras/ErdosPentagon/ErdosPentagon.lean
@@ -10,7 +10,7 @@ example
   flagQuadraticForm_nonneg R_real R_real_posSemidef v₂
 
 example
-    : flagQuadraticForm R_real Vᵣ =[K3]
+    : flagQuadraticForm R_real v₂ =[K3]
         (1512 / 625 : ℝ) • FlagAlgebra_5_3_2_0
         - (380 / 625 : ℝ) • FlagAlgebra_5_3_2_1
         + (568 / 625 : ℝ) • FlagAlgebra_5_3_2_2
